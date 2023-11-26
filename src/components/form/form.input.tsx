fed4d31896e2ac3efa296565f2e46ba3062edace
Misho <chapidze.misho95@gmail.com>
2023-11-26 11:09:17 +0400

    s

--- src/components/form/form.input.tsx
+++ src/components/form/form.input.tsx
@@ -1,19 +1,40 @@
+import { InputType } from "../../pages/signup.page";
+import { MdErrorOutline } from "react-icons/md";
+
 interface PropsType {
   title: string;
   type: string;
   placeHolder: string;
+  input: InputType;
+  setInput: (arg: InputType) => void;
 }
 
-const FormInput = ({ title, type, placeHolder }: PropsType) => {
+const FormInput = ({
+  title,
+  type,
+  placeHolder,
+  input,
+  setInput,
+}: PropsType) => {
   return (
-    <label className="flex flex-col gap-[5px] w-full">
+    <label className="flex flex-col gap-[5px] w-full relative">
       <div className="text-white/70 ml-2">{title}</div>
       <input
+        value={input.value}
+        onChange={(e) => setInput({ value: e.target.value, error: null })}
         type={type}
         placeholder={placeHolder}
         className="w-full h-[40px] border-[1px] border-transparent p-2 bg-red-800/80 focus:bg-red-800 focus:border-white/30 text-red-200 placeholder:text-red-300 focus:outline-none rounded-lg duration-200"
         autoComplete={"on"}
       />
+      <div className=" text-red-400 absolute right-[10px] bottom-[8px] flex gap-1 items-center justify-center">
+        {input.error && (
+          <>
+            <MdErrorOutline />
+            <span className="italic">{input.error}</span>
+          </>
+        )}
+      </div>
     </label>
   );
 };
--- src/pages/signup.page.tsx
+++ src/pages/signup.page.tsx
@@ -3,6 +3,13 @@ import samurai from "../assets/samurai.jpg";
 import FormButton from "../components/form/form.button";
 import FormInput from "../components/form/form.input";
 import { animated, useSpring } from "@react-spring/web";
+import { useState } from "react";
+import axios from "axios";
+
+export interface InputType {
+  value: string;
+  error: null | string;
+}
 
 const SingUpPage = () => {
   const animateFormContainer = useSpring({
@@ -15,6 +22,93 @@ const SingUpPage = () => {
     },
   });
 
+  const [firstName, setFirstName] = useState<InputType>({
+    value: "",
+    error: null,
+  });
+  const [userName, setUserName] = useState<InputType>({
+    value: "",
+    error: null,
+  });
+  const [email, setEmail] = useState<InputType>({
+    value: "",
+    error: null,
+  });
+  const [password, setPassword] = useState<InputType>({
+    value: "",
+    error: null,
+  });
+  const [rePassword, setRePassword] = useState<InputType>({
+    value: "",
+    error: null,
+  });
+
+  const submitSignUpForm = (e) => {
+    e.preventDefault();
+
+    let value = 0;
+
+    if (firstName.value === "") {
+      setFirstName({ value: firstName.value, error: "empty!" });
+    } else {
+      setFirstName({ value: firstName.value, error: null });
+      value += 1;
+    }
+
+    if (userName.value === "") {
+      setUserName({ value: userName.value, error: "empty!" });
+    } else {
+      setUserName({ value: userName.value, error: null });
+      value += 1;
+    }
+
+    if (email.value === "") {
+      setEmail({ value: email.value, error: "empty!" });
+    } else {
+      setEmail({ value: email.value, error: null });
+      value += 1;
+    }
+
+    if (password.value === "") {
+      setPassword({ value: password.value, error: "empty!" });
+    } else {
+      setPassword({ value: password.value, error: null });
+      value += 1;
+    }
+
+    if (rePassword.value === "") {
+      setRePassword({ value: rePassword.value, error: "empty!" });
+    } else {
+      setRePassword({ value: rePassword.value, error: null });
+      value += 1;
+    }
+
+    if (value < 5) {
+      return;
+    }
+
+    if (password.value !== rePassword.value) {
+      setRePassword({ value: rePassword.value, error: "Not Match!" });
+      return;
+    } else {
+      setRePassword({ value: rePassword.value, error: null });
+    }
+
+    axios
+      .post("https://budget-app-bz54x.ondigitalocean.app/api/auth/signup", {
+        firstName: firstName.value,
+        username: userName.value,
+        email: email.value,
+        password: password.value,
+      })
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
+
   return (
     <div className="w-full flex justify-center">
       <animated.div
@@ -26,31 +120,44 @@ const SingUpPage = () => {
           <h1 className="text-[30px] text-white select-none w-full text-center">
             SignUp
           </h1>
-          <form className="p-[20px] w-full h-fit flex flex-col gap-[10px] items-center ">
+          <form
+            onSubmit={submitSignUpForm}
+            className="p-[20px] w-full h-fit flex flex-col gap-[10px] items-center "
+          >
             <FormInput
               title={"FirstName"}
               type={"text"}
               placeHolder={"FirstName"}
+              input={firstName}
+              setInput={setFirstName}
             />
             <FormInput
               title={"UserName"}
               type={"text"}
               placeHolder={"UserName"}
+              input={userName}
+              setInput={setUserName}
             />
             <FormInput
               title={"Email"}
               type={"email"}
               placeHolder={"example@mail.com"}
+              input={email}
+              setInput={setEmail}
             />
             <FormInput
               title={"Password"}
               type={"password"}
               placeHolder={"********"}
+              input={password}
+              setInput={setPassword}
             />
             <FormInput
               title={"Re-Password"}
-              type={"repassword"}
+              type={"password"}
               placeHolder={"********"}
+              input={rePassword}
+              setInput={setRePassword}
             />
 
             <div className="text-white/50 flex gap-[10px]">
