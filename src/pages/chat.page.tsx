0ec779d722e225415b0a896c129ab02ffeaad87e
Misho <chapidze.misho95@gmail.com>
2023-12-09 14:40:57 +0400

    sx

--- src/pages/chat.page.tsx
+++ src/pages/chat.page.tsx
@@ -32,8 +32,7 @@ const ChatPage = () => {
 
   const [socket, setSocket] = useState<Socket | null>(null);
   const [message, setMessage] = useState("");
-  // const [chatLog, setChatLog] = useState<MessageType[]>([]);
-  const [chatLog, setChatLog] = useState<string[]>([]);
+  const [chatLog, setChatLog] = useState<MessageType[]>([]);
   const chatContainer = useRef<HTMLDivElement | null>(null);
 
   useLayoutEffect(() => {
@@ -42,42 +41,6 @@ const ChatPage = () => {
     }
   }, []);
 
-  useEffect(() => {
-    const newSocket = io("ws://localhost:8080");
-    setSocket(newSocket);
-
-    return () => {
-      if (newSocket.connected) {
-        newSocket.disconnect();
-      }
-    };
-  }, []);
-
-  useEffect(() => {
-    if (socket) {
-      socket.emit("login", user?._id);
-      socket.on("message", (msg: string) => {
-        console.log(msg);
-        setChatLog((prevChatLog) => [...prevChatLog, msg]);
-        // axiosInstance
-        //   .post(`/api/chat/${userId}`, { message: msg })
-        //   .then((res) => {
-        //     setChatLog((prevChatLog) => [...prevChatLog, res.data]);
-        //   })
-        //   .catch((err) => {
-        //     console.log(err);
-        //   });
-      });
-    }
-
-    return () => {
-      // Clean up socket event listeners here if needed
-      if (socket) {
-        socket.off("message");
-      }
-    };
-  }, [socket]);
-
   const scrollDown = () => {
     if (!chatContainer.current) {
       return;
@@ -85,6 +48,25 @@ const ChatPage = () => {
 
     chatContainer.current.scrollTop = chatContainer.current.scrollHeight;
   };
+
+  useEffect(() => {
+    const newSocket = io("ws://localhost:8080");
+    setSocket(newSocket);
+
+    return () => {
+      if (socket?.connected) {
+        socket.disconnect();
+      }
+    };
+  }, []);
+
+  useEffect(() => {
+    if (socket) {
+      socket.on("message", (res) => {
+        setChatLog((prevChatLog) => [...prevChatLog, res.data]);
+      });
+    }
+  }, [socket]);
 
   useEffect(() => {
     scrollDown();
@@ -104,18 +86,25 @@ const ChatPage = () => {
   const sendMessage = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     if (socket && message.trim() !== "") {
-      socket.emit("message", { message, userId: user?._id });
+      axiosInstance
+        .post(`/api/chat/${userId}`, { message })
+        .then((res) => {
+          socket.emit("message", { message, data: res.data });
+        })
+        .catch((err) => {
+          console.log(err);
+        });
       setMessage("");
     }
   };
 
-  // const checkForPrevChat = (index: number, idToCheck: string | undefined) => {
-  //   if (chatLog[index]?.sendFrom !== idToCheck) {
-  //     return true;
-  //   } else {
-  //     return false;
-  //   }
-  // };
+  const checkForPrevChat = (index: number, idToCheck: string | undefined) => {
+    if (chatLog[index]?.sendFrom !== idToCheck) {
+      return true;
+    } else {
+      return false;
+    }
+  };
 
   return (
     <animated.div style={{ ...animatedPage }} className="flex justify-center">
@@ -124,10 +113,36 @@ const ChatPage = () => {
           ref={chatContainer}
           className="h-[500px] overflow-y-auto flex flex-col gap-[5px]"
         >
-          {chatLog.map((chat, index) => (
+          {chatLog.map((chat, index) => {
+            return (
+              <div
+                key={chat._id}
+                className={`flex flex-col gap-[5px] w-full ${
+                  chat.sendFrom === user?._id ? "self-start" : "self-end"
+                }`}
+              >
+                {checkForPrevChat(index - 1, chat.sendFrom) && (
+                  <h6
+                    className={`text-[12px] text-black/60 ${
+                      chat.sendFrom === user?._id ? "self-start" : "self-end"
+                    }`}
+                  >
+                    {chat.userName}
+                  </h6>
+                )}
 
-           return <div>sss</div>;
-          ))}
+                <div
+                  className={`w-fit max-w-[200px]  p-[5px] rounded-lg text-white break-words ${
+                    chat.sendFrom === user?._id
+                      ? "bg-indigo-500 self-start"
+                      : "bg-pink-500 self-end"
+                  }`}
+                >
+                  {chat.message}
+                </div>
+              </div>
+            );
+          })}
         </div>
         <form
           onSubmit={sendMessage}
@@ -150,32 +165,3 @@ const ChatPage = () => {
 };
 
 export default ChatPage;
-
-
-
-// <div
-// key={chat._id}
-// className={`flex flex-col gap-[5px] w-full ${
-//   chat.sendFrom === user?._id ? "self-start" : "self-end"
-// }`}
-// >
-// {checkForPrevChat(index - 1, chat.sendFrom) && (
-//   <h6
-//     className={`text-[12px] text-black/60 ${
-//       chat.sendFrom === user?._id ? "self-start" : "self-end"
-//     }`}
-//   >
-//     {chat.userName}
-//   </h6>
-// )}
-
-// <div
-//   className={`w-fit max-w-[200px]  p-[5px] rounded-lg text-white break-words ${
-//     chat.sendFrom === user?._id
-//       ? "bg-indigo-500 self-start"
-//       : "bg-pink-500 self-end"
-//   }`}
-// >
-//   {chat.message}
-// </div>
-// </div>
