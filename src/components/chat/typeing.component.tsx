dbb8fb7f3f04ac3653a6db341d88143a771d3fef
Misho <chapidze.misho95@gmail.com>
2023-12-13 20:31:41 +0400

    fxd

--- src/components/chat/typeing.component.tsx
+++ src/components/chat/typeing.component.tsx
@@ -1,21 +1,8 @@
-import { useEffect } from "react";
-import typeingSound from "../../assets/typing.mp3";
 import { TiMessageTyping } from "react-icons/ti";
 
 const TypeingComponent = () => {
-  useEffect(() => {
-    const audio = new Audio(typeingSound);
-    audio.loop = true;
-    audio.play();
-
-    return () => {
-      audio.pause();
-      audio.currentTime = 0;
-    };
-  }, []);
-
   return (
-    <div className="bg-neutral-300 w-fit h-fit py-[5px] px-[10px] rounded-full animate-pulse flex gap-[5px] text-neutral-800 justify-center items-center select-none font-mono scale-90">
+    <div className="bg-neutral-300 w-fit h-fit py-[5px] px-[10px] rounded-full animate-pulse flex gap-[5px] text-neutral-800 justify-center items-center select-none font-mono scale-90 italic">
       <TiMessageTyping className={"w-[20px] h-[20px] text-neutral-800"} />
       <p>Typing...</p>
     </div>
