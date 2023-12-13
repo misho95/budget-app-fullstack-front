939aee3bd6eb145a91a0d06acbaa8c6f9892de2c
Misho <chapidze.misho95@gmail.com>
2023-12-13 20:42:27 +0400

    fxd

--- src/components/chat/typeing.component.tsx
+++ src/components/chat/typeing.component.tsx
@@ -22,7 +22,7 @@ const TypeingComponent = () => {
   }, []);
 
   return (
-    <div className="bg-neutral-300 w-fit h-fit py-[5px] px-[10px] rounded-full animate-pulse flex gap-[5px] text-neutral-800 justify-center items-center select-none font-mono scale-90">
+    <div className="bg-neutral-300 w-fit h-fit py-[5px] px-[10px] rounded-full animate-pulse flex gap-[5px] text-neutral-800 justify-center items-center select-none font-mono scale-90 italic">
       <TiMessageTyping className={"w-[20px] h-[20px] text-neutral-800"} />
       <p>Typing...</p>
     </div>
