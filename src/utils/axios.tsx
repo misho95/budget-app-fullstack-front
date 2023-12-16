e434159b5628d3263199ec1b94e58187ad90bcf2
Misho <chapidze.misho95@gmail.com>
2023-12-16 10:44:48 +0400

    s

--- src/utils/axios.tsx
+++ src/utils/axios.tsx
@@ -3,7 +3,7 @@ import axios from "axios";
 const url = "https://budget-app-bz54x.ondigitalocean.app";
 
 const axiosInstance = axios.create({
-  baseURL: url,
+  baseURL: "http://localhost:8080/",
 });
 
 export default axiosInstance;
